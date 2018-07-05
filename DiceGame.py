7a672bce285460554d32d55d203d717bd9bbcfff
s1250119 <s1250119@std6dc38.u-aizu.ac.jp>
2018-07-05 19:37:04 +0900

    Won/lost message added

--- DiceGame.py
+++ DiceGame.py
@@ -1,5 +1,9 @@
 from random import Random
 
+print("What is your name?")
+name = raw_input('> ')
+print("Hello, ") + name + ("!")
+
 print("Rolling the dice...")
 r = Random()
 x = r.randint(1,6)
@@ -7,4 +11,7 @@ y = r.randint(1,6)
 print("Die 1: ") + str(x)
 print("Die 2: ") + str(y)
 print("Total value: ") + str(x+y)
-
+if x+y > 7:
+    print name + (" won!")
+else:
+    print name + (" lost!")
